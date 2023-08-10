6a570d0a278624af63d0fe95999deec4aaee0c7f
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-10 17:29:07 -0300

    Lado direito hero section

--- components/Hero/Direita.jsx
+++ components/Hero/Direita.jsx
@@ -1,7 +1,10 @@
 export default function HeroDireita(){
     return (
-        <div className="h-32 flex-1 bg-green-400">
-            
+        <div className="flex-1 relative h-full overflow-hidden">
+            <div className="w-96 elementColorPrimary h-96 absolute rounded-full bottom-0 -mb-8 ml-14"></div>
+            <div className="w-96 elementColorPrimary h-96 absolute rounded-full bottom-0 mb-6 ml-36 opacity-40"></div>
+            <div className="w-96 elementColorPrimary h-96 absolute rounded-full bottom-0 mb-8 ml-56 opacity-20"></div>
+            <div className="w-50 h-full bg-red-700"></div>
         </div>
     )
 }
