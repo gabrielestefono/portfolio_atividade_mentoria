5b107c670fe3b30787cd819bfa0bcddbb5812adb
gabrielestefono <gabrielestefono@hotmail.com>
2023-08-13 23:33:49 -0300

    Corrigindo e inserindo o elemento

--- components/Hero/Direita.jsx
+++ components/Hero/Direita.jsx
@@ -4,7 +4,7 @@ export default function HeroDireita(){
             <div className="w-96 elementColorPrimary h-96 absolute rounded-full bottom-0 -mb-8 ml-14"></div>
             <div className="w-96 elementColorPrimary h-96 absolute rounded-full bottom-0 mb-6 ml-36 opacity-40"></div>
             <div className="w-96 elementColorPrimary h-96 absolute rounded-full bottom-0 mb-8 ml-56 opacity-20"></div>
-            <div className="w-50 h-full bg-red-700"></div>
+            <div className="w-50 h-full"></div>
         </div>
     )
 }
--- components/Hero/HeroSection.jsx
+++ components/Hero/HeroSection.jsx
@@ -1,11 +1,15 @@
 import HeroDireita from "./Direita";
 import HeroEsquerda from "./Esquerda";
+import HeroInfo from "./Info";
 
 export default function Hero(){
     return (
-        <div className="max-w-screen-2xl flex">
-            <HeroEsquerda/>
-            <HeroDireita/>
-        </div>
+        <>
+            <div className="max-w-screen-2xl flex">
+                <HeroEsquerda/>
+                <HeroDireita/>
+            </div>
+            <HeroInfo/>
+        </>
     )
 }
